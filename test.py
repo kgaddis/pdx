fa3bc389e5bfb365288c14c87e61b4ae3baedad7
Kainoa Gaddis <gaddis@pdx.edu>
2014-07-21 14:12:35 -0700

    Add test file

---
+++ test.py
@@ -0,0 +1,12 @@
+# Kainoa Gaddis (c) 2014
+# first repeadted element
+
+def first_repeated(s):
+    m = []
+    for e in s:
+        if e not in m:
+            m.append(e)
+        else:
+            print(e)
+            return e
+
