7c534d8dbe01b0a23a571d31857096c7b85f290c
Kainoa Gaddis <gaddis@pdx.edu>
2014-07-28 13:21:49 -0700

    adding die roll program

---
+++ die_roll.py
@@ -0,0 +1,29 @@
+# Kainoa Gaddis (c) 2014
+# Display die roll
+
+from random import *
+from tkinter import *
+
+class DieRoller(Frame):
+
+    def __init__(self, root):
+        super().__init__(root)
+        self.grid()
+        self.display = Label(self, width = 1)
+        self.display.grid(row = 0, column = 0, sticky E + W)
+        self.roll = Button(self, text = "Roll", command = self.do_roll)
+        self.roll.grid(row = 1, column = 0, sticky = E + W)
+
+    def do_roll(self):
+        pass
+
+
+# Seed the PRNG
+seed()
+
+
+# Start the app
+root = Tk()
+root.title("Die Roller")
+app = DieRoller(root)
+root.mainloop()
